9f4c201c180bdd063e5dd427f68c2b1ed63a5e29
Lathihoaihoa <hoa.lth.60cntt@ntu.edu.vn>
2022-01-01 10:21:44 +0700

    initial project version

---
+++ uploads/cache/039409be34eea8f96a0f95a7ef6fdafcb56d8edf.php
@@ -0,0 +1,10 @@
+<article class="bg-red-500" <?php post_class() ?>>
+    <header>
+        HAHAHA
+        <h2 class="entry-title"><a href="<?php echo e(get_permalink()); ?>"><?php echo get_the_title(); ?></a></h2>
+        <?php echo $__env->make('partials/entry-meta', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
+    </header>
+    <div class="entry-summary">
+        <?php the_excerpt() ?>
+    </div>
+</article>
---
+++ uploads/cache/9ab4c891a8304af5bc2d8b44e9002c084d26df43.php
@@ -0,0 +1,67 @@
+<footer id="footer" class="module footer bg-[#2c2c2c]">
+    <div class="container flex flex-col flex-wrap px-4 py-16 mx-auto 2xl:flex-row">
+        <div class="w-full mt-4 lg:flex">
+            <div class="w-full px-4 2xl:w-1/3">
+                <h3 class="mb-2 font-semibold tracking-widest text-white">
+                    LIÊN LẠC
+                </h3>
+                <hr>
+                <ul class="text-base py-8 px-0 list-none text-white">
+                    <li>
+                        <a class="text-white hover:text-gray-300" href="">thien@gmail.com</a>
+                    </li>
+                    <li>
+                        <a class="text-white hover:text-gray-300" href="">095253524</a>
+                    </li>
+                    <li>
+                        <a class="text-white hover:text-gray-300" href="">Facebook</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="w-full px-4 2xl:w-1/3">
+                <h3 class="mb-2 font-semibold tracking-widest text-white">
+                    CÔNG TY CHÚNG TÔI
+                </h3>
+                <hr>
+                <ul class="text-base py-8 px-0 list-none text-white">
+                    <li>
+                        <a class="text-white hover:text-gray-300" href="#">Tiểu sử</a>
+                    </li>
+                    <li>
+                        <a class="text-white hover:text-gray-300" href="#">Tuyển dụng</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="w-full px-4 2xl:w-1/3">
+                <h3 class="mb-2 font-semibold tracking-widest text-white">
+                    SẢN PHẨM
+                </h3>
+                <hr>
+                <ul class="text-base py-8 px-0 list-none text-white">
+                    <li>
+                        <a class="text-white hover:text-gray-300" href="#">Quần</a>
+                    </li>
+                    <li>
+                        <a class="text-white hover:text-gray-300" href="#">Áo</a>
+                    </li>
+                    <li>
+                        <a class="text-white hover:text-gray-300" href="#">Giày</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<!-- endblock -->
+<noscript>
+    <div id="mod-noscript" class="mod-noscript bg-black text-white fixed inset-0 z-200">
+        <div class="table w-full h-full">
+            <div class="table-cell align-middle text-center">
+                <div class="container last-mb-none">
+                    <h3>To use web better, please enable Javascript.</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+</noscript>
